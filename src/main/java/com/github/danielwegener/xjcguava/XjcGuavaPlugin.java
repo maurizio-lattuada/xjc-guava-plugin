87fd96541f6ded03d6ad26591bf9a35c1a830ea5
Maurizio Lattuada <maurizio.lattuada@vivates.ch>
2016-07-25 13:33:35 +0200

    Now using MoreObjects of Guava. Bumped up some libraries.

--- src/main/java/com/github/danielwegener/xjcguava/XjcGuavaPlugin.java
+++ src/main/java/com/github/danielwegener/xjcguava/XjcGuavaPlugin.java
@@ -1,12 +1,9 @@
 /*
  * Copyright 2013 Daniel Wegener
- *
  * Licensed under the Apache License, Version 2.0 (the "License")
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,17 +22,20 @@ import org.xml.sax.ErrorHandler;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Stream;
 
 /**
- * <p>Generates hashCode, equals and toString methods using Guavas Objects helper class.</p>
+ * <p>
+ * Generates hashCode, equals and toString methods using Guavas Objects helper class.
+ * </p>
  *
  * @author Daniel Wegener
  */
 public class XjcGuavaPlugin extends Plugin {
 
     public static final String OPTION_NAME = "Xguava";
-    public static final String SKIP_TOSTRING_PARAM = "-"+OPTION_NAME + ":skipToString";
-
+    public static final String SKIP_TOSTRING_PARAM = "-" + OPTION_NAME + ":skipToString";
+    private boolean skipToString = false;
 
     @Override
     public String getOptionName() {
@@ -44,15 +44,15 @@ public class XjcGuavaPlugin extends Plugin {
 
     @Override
     public String getUsage() {
-        return "  -" + OPTION_NAME + "\t:  enable generation of guava toString, equals and hashCode methods"
-             + "\n    -" + SKIP_TOSTRING_PARAM + "\t:  dont wrap collection parameters with Collections.unmodifiable...";
+        return "  -" + OPTION_NAME + "\t:  enable generation of guava toString, equals and hashCode methods" + "\n    -"
+                + SKIP_TOSTRING_PARAM + "\t:  dont wrap collection parameters with Collections.unmodifiable...";
 
     }
 
     @Override
     public int parseArgument(Options opt, String[] args, int i) throws BadCommandLineException, IOException {
-
         final String arg = args[i].trim();
+
         if (SKIP_TOSTRING_PARAM.equals(arg)) {
             skipToString = true;
             return 1;
@@ -60,25 +60,20 @@ public class XjcGuavaPlugin extends Plugin {
         return 0;
     }
 
-    private boolean skipToString = false;
-
     @Override
     public boolean run(final Outline outline, final Options options, final ErrorHandler errorHandler) {
         // For each defined class
         final JCodeModel model = outline.getCodeModel();
         for (final ClassOutline classOutline : outline.getClasses()) {
-
             final JDefinedClass implClass = classOutline.implClass;
-
             if (!skipToString && !implClass.isAbstract() && implClass.getMethod("toString", new JType[0]) == null) {
                 generateToStringMethod(model, implClass);
             }
-
             if (!implClass.isAbstract()) {
-                if (implClass.getMethod("hashCode",new JType[0]) == null)
+                if (implClass.getMethod("hashCode", new JType[0]) == null)
                     generateHashCodeMethod(model, implClass);
-                if (implClass.getMethod("equals",new JType[]{model._ref(Object.class)}) == null) {
-                    generateEqualsMethod(model,implClass);
+                if (implClass.getMethod("equals", new JType[] { model._ref(Object.class) }) == null) {
+                    generateEqualsMethod(model, implClass);
                 }
             }
         }
@@ -86,61 +81,45 @@ public class XjcGuavaPlugin extends Plugin {
     }
 
     protected void generateToStringMethod(JCodeModel model, JDefinedClass clazz) {
-        final JMethod toStringMethod = clazz.method(JMod.PUBLIC, String.class,"toString");
+        final JMethod toStringMethod = clazz.method(JMod.PUBLIC, String.class, "toString");
         toStringMethod.annotate(Override.class);
-        final JClass objects = model.ref("com.google.common.base.Objects");
+        final JClass objects = model.ref("com.google.common.base.MoreObjects");
         final Collection<JFieldVar> superClassInstanceFields = getInstanceFields(getSuperclassFields(clazz));
         final Collection<JFieldVar> thisClassInstanceFields = getInstanceFields(clazz.fields().values());
-
         final JBlock content = toStringMethod.body();
-
-
         final JInvocation toStringHelperCall = objects.staticInvoke("toStringHelper");
+
         toStringHelperCall.arg(JExpr._this());
         JInvocation fluentCall = toStringHelperCall;
-
         for (JFieldVar superField : superClassInstanceFields) {
             fluentCall = fluentCall.invoke("add");
             fluentCall.arg(JExpr.lit(superField.name()));
             fluentCall.arg(superField);
         }
-
         for (JFieldVar thisField : thisClassInstanceFields) {
             fluentCall = fluentCall.invoke("add");
             fluentCall.arg(JExpr.lit(thisField.name()));
             fluentCall.arg(thisField);
         }
-
         fluentCall = fluentCall.invoke("toString");
-
         content._return(fluentCall);
-
     }
 
     protected void generateHashCodeMethod(JCodeModel model, JDefinedClass clazz) {
-
         final JClass objects = model.ref(com.google.common.base.Objects.class);
         final Collection<JFieldVar> thisClassInstanceFields = getInstanceFields(clazz.fields().values());
         final Collection<JFieldVar> superClassInstanceFields = getInstanceFields(getSuperclassFields(clazz));
+
         // Don't create hashCode for empty classes
-        if (thisClassInstanceFields.isEmpty() && superClassInstanceFields.isEmpty()) return;
-
-
-        final JMethod hashCodeMethod = clazz.method(JMod.PUBLIC, model.INT ,"hashCode");
+        if (thisClassInstanceFields.isEmpty() && superClassInstanceFields.isEmpty()) {
+            return;
+        }
+        final JMethod hashCodeMethod = clazz.method(JMod.PUBLIC, model.INT, "hashCode");
         final JBlock content = hashCodeMethod.body();
         hashCodeMethod.annotate(Override.class);
-
         final JInvocation hashCodeCall = objects.staticInvoke("hashCode");
-
-
-        for (JFieldVar superField : superClassInstanceFields) {
-            hashCodeCall.arg(superField);
-        }
-
-        for (JFieldVar thisField : thisClassInstanceFields) {
-            hashCodeCall.arg(thisField);
-        }
-
+        superClassInstanceFields.stream().forEachOrdered(superField -> hashCodeCall.arg(superField));
+        thisClassInstanceFields.stream().forEachOrdered(thisField -> hashCodeCall.arg(thisField));
         content._return(hashCodeCall);
     }
 
@@ -148,11 +127,12 @@ public class XjcGuavaPlugin extends Plugin {
         final Collection<JFieldVar> superClassInstanceFields = getInstanceFields(getSuperclassFields(clazz));
         final Collection<JFieldVar> thisClassInstanceFields = getInstanceFields(clazz.fields().values());
         // Don't create equals for empty classes
-        if (thisClassInstanceFields.isEmpty() && superClassInstanceFields.isEmpty()) return;
+        if (thisClassInstanceFields.isEmpty() && superClassInstanceFields.isEmpty())
+            return;
 
-        final JMethod equalsMethod = clazz.method(JMod.PUBLIC, model.BOOLEAN ,"equals");
+        final JMethod equalsMethod = clazz.method(JMod.PUBLIC, model.BOOLEAN, "equals");
         equalsMethod.annotate(Override.class);
-        final JVar other = equalsMethod.param(Object.class,"other");
+        final JVar other = equalsMethod.param(Object.class, "other");
 
         final JClass objects = model.ref(com.google.common.base.Objects.class);
         final JClass arrays = model.ref(java.util.Arrays.class);
@@ -199,19 +179,17 @@ public class XjcGuavaPlugin extends Plugin {
         content._return(equalsBuilder);
     }
 
-
-
     /**
      * Takes a collection of fields, and returns a new collection containing only the instance
      * (i.e. non-static) fields.
      */
     protected Collection<JFieldVar> getInstanceFields(final Collection<JFieldVar> fields) {
         final List<JFieldVar> instanceFields = new ArrayList<JFieldVar>();
-        for (final JFieldVar field : fields) {
+        fields.stream().forEachOrdered(field -> {
             if (!isStatic(field)) {
                 instanceFields.add(field);
             }
-        }
+        });
         return instanceFields;
     }
 
@@ -223,12 +201,10 @@ public class XjcGuavaPlugin extends Plugin {
             fieldList.addAll(0, ((JDefinedClass) superclass).fields().values());
             superclass = superclass._extends();
         }
-
         return fieldList;
     }
 
     protected boolean isStatic(final JFieldVar field) {
-        final JMods fieldMods = field.mods();
-        return (fieldMods.getValue() & JMod.STATIC) > 0;
+        return (field.mods().getValue() & JMod.STATIC) > 0;
     }
 }
